dda7b49dd91cc505288e0b808effdd8d5d5830da
Dhruv Singal <dhruvsingalabc@gmail.com>
2018-04-08 17:59:47 -0700

    parsing completed. Now for some Math

--- lab1/NNRun.py
+++ lab1/NNRun.py
@@ -1,11 +1,16 @@
 import numpy as np
 import sys
-NMatrix, values, dataInput={},[],[]
-print(sys.argv[1])
+NMatrix, Nweight, values, dataInput,values,data=[],[],[],[],[],input('enter file name')
 for line in open(sys.argv[1]):
-    if(line.strip()):values.extend((list(filter(lambda x: x!='',(line.lstrip().rstrip().split(sep=' '))))))
-#numinputs,numlayers=values.pop(0),values.pop(0)
-data=input('enter file name')
+    if(line.strip()):values.extend(list(map(int,list(filter(lambda x: x!='',(line.lstrip().rstrip().split(sep=' ')))))))
 for line in open(data):
-    if(line.strip()):dataInput.append((list(filter(lambda x: x!='',(line.lstrip().rstrip().split(sep=' '))))))
-print(np.asarray(dataInput))
+    if(line.strip()):dataInput.append(list(map(float,list(filter(lambda x: x!='',(line.lstrip().rstrip().split(sep=' ')))))))
+numinputs,numlayers=values.pop(0),values.pop(0)
+for i in range(numlayers):
+    numNeurons=values.pop(0)
+    for i in range(numNeurons):
+        NMatrix,values=NMatrix+values[0:numinputs+1],values[numinputs+1:]
+    Nweight.append(np.asarray(NMatrix).reshape(numNeurons,numinputs+1))
+    NMatrix,numinputs=[],numNeurons
+print(Nweight)
+print(dataInput)
