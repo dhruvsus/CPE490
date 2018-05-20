4bdca053bff92627092255b8c98a30392ea68404
DhruvSUS <dhruvsingalabc@gmail.com>
2018-05-20 03:52:19 -0700

    loaded up Embedding.dat into a numpy array

--- lab4/AnalyzeEmbedding.py
+++ lab4/AnalyzeEmbedding.py
@@ -10,9 +10,5 @@ for word in open(vocab).readlines():
     vocab_sequence.append(word)
 vocab_sequence = np.asarray(vocab_sequence)
 # load embedding into a numpy array
-for embed in open(embedding).readlines():
-    embedding_sequence.append(np.asarray(embed))
-embedding_sequence = np.asarray(embedding_sequence)
-print(embedding_sequence)
-print(embedding_sequence.shape)
-print(embedding_sequence.size)
+embedding_sequence=np.loadtxt(embedding)
+
